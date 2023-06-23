fc3e167377945381fdcf9670609245ef2db5c012
drcpu <drcpu@protonmail.com>
2023-06-23 20:51:53 +0200

    Add random witness selection option

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-### Tools to calculate different Proof-of-Stake metrics
+## Tools to calculate different Proof-of-Stake metrics
 
-#### test_mining_eligibility.py
+### test_mining_eligibility.py
 ./test_mining_eligibility.py [--stakers int] [--distribution str] [--epochs int] [--replication int]
 
 Accepted arguments for the staking distribution: gauss, uniform, beta, exponential, gamma
+
+### test_witness_attack.py
+
+./test_witness_attack.py [--staker int] [--largest-staker int] [--stake-per-node int] [--total-staked int] [--data-requests int] [--witnesses int] [--random]
+
+random: use random eligibility for data request witnessing instead of stake ratio
--- test_witness_attack.py
+++ test_witness_attack.py
@@ -13,13 +13,20 @@ def build_stakers(num_stakers, largest_staker, stake_per_node, total_staked):
     stakers.extend([stake_per_node / total_staked for i in range(int(largest_staker / stake_per_node))])
     return stakers
 
-def simulate_data_request(stakers, witnesses, attacker_nodes):
+def simulate_data_request(stakers, witnesses, attacker_nodes, random_selection):
     # eligibility: vrf < 2 ^ 256 * own_power / global_power * witnesses * 2 ^ round
     others_eligibility = [0, 0, 0, 0]
     attacker_eligiblity = [0, 0, 0, 0]
     for i in range(0, 4):
         for j, staker in enumerate(stakers):
-            if random.random() < staker * witnesses * (2 ** i):
+            # Eligibility is completely random:
+            if random_selection:
+                eligibility = 1 / len(stakers) * witnesses * (2 ** i)
+            # Eligibility is proportional to the staker's balance
+            else:
+                eligibility = staker * witnesses * (2 ** i)
+
+            if random.random() < eligibility:
                 if j >= len(stakers) - attacker_nodes:
                     assert stakers[j] == 0.01
                     attacker_eligiblity[i] += 1
@@ -35,6 +42,7 @@ def main():
     parser.add_option("--total-staked", type="int", default=100000000, dest="total_staked")
     parser.add_option("--data-requests", type="int", default=100000, dest="data_requests")
     parser.add_option("--witnesses", type="int", default=10, dest="witnesses")
+    parser.add_option("--random", action="store_true", dest="random")
     options, args = parser.parse_args()
 
     stakers = build_stakers(options.stakers, options.largest_staker, options.stake_per_node, options.total_staked)
@@ -43,7 +51,7 @@ def main():
     successful_data_request_attacks = [0, 0, 0, 0]
     attacker_nodes = int(options.largest_staker / options.stake_per_node)
     for epoch in tqdm.tqdm(range(options.data_requests)):
-        others_eligibility, attacker_eligiblity = simulate_data_request(stakers, options.witnesses, attacker_nodes)
+        others_eligibility, attacker_eligiblity = simulate_data_request(stakers, options.witnesses, attacker_nodes, options.random)
         for i, (oe, ae) in enumerate(zip(others_eligibility, attacker_eligiblity)):
             if oe >= options.witnesses:
                 successful_data_requests[i] += 1
@@ -55,7 +63,7 @@ def main():
         success = so / options.data_requests
         print(f"Successful data requests @ commit round {i + 1}: {success * 100:.2f}%")
         successful_attack = sa / options.data_requests
-        print(f"Successful data request attacks @ commit round {i + 1}: {successful_attack * 100:.2f}%")
+        print(f"Successful data request attack @ commit round {i + 1}: {successful_attack * 100:.2f}%")
         dependent_successful_attack = (1 - previous_success) * successful_attack
         print(f"Dependent successful data request attacks @ commit round {i + 1}: {dependent_successful_attack * 100:.2f}%")
         print("")
