9b3fda163a30349e8e8e8e08de0ee79d78c0f22f
drcpu <drcpu@protonmail.com>
2023-07-01 08:04:32 +0200

    Add features and bug fixes to the mining simulator script

--- README.md
+++ README.md
@@ -10,3 +10,39 @@ Accepted arguments for the staking distribution: gauss, uniform, beta, exponenti
 ./test_witness_attack.py [--staker int] [--largest-staker int] [--stake-per-node int] [--total-staked int] [--data-requests int] [--witnesses int] [--random]
 
 random: use random eligibility for data request witnessing instead of stake ratio
+
+### simulate_mining.py
+./simulate_mining.py [options]
+
+Options:
+
+- --stakers
+    - explanation: number of total stakers to simulate
+    - default: 100
+- --total-staked
+    - explanation: total amount staked
+    - default: 100000000
+- --distribution
+    - explanation: defines the distribution of the amount each staker (relative to the total amount)
+    - default: random
+    - allowed values: random, uniform, beta, exponential, gamma
+- --whales
+    - explanation: adds a number of whale stakers to the staking distribution
+    - default: 0
+- --whales-stake
+    - explanation: percentage of the total staked to divide uniformly over the number of whales
+    - default: 0
+- --epochs
+    - explanation: the number of epochs for which to run the mining simulator
+    - default: 100000
+- --replication
+    - explanation: set the replication factor to approximate the number of blocks generated per epoch
+    - default: 16
+- --coin-ageing
+    - explanation: allows to experiment with different coin age strategies
+    - default: reset
+    - allowed values: reset, capped, halving
+- --logging
+    - explanation: set the logging level
+    - default: info
+    - allowed values: debug, info, warning, error, critical
--- simulate_mining.py
+++ simulate_mining.py
@@ -1,14 +1,57 @@
 #!/usr/bin/python3
 
+import datetime
+import logging
 import optparse
 import os
 import random
 import sys
+import time
 import tqdm
 
 import matplotlib.pyplot as plt
 
-def build_stakers(num_stakers, total_staked, distribution):
+def select_logging_level(level):
+    if level.lower() == "debug":
+        return logging.DEBUG
+    elif level.lower() == "info":
+        return logging.INFO
+    elif level.lower() == "warning":
+        return logging.WARNING
+    elif level.lower() == "error":
+        return logging.ERROR
+    elif level.lower() == "critical":
+        return logging.CRITICAL
+    else:
+        print(f"Unknown logging level: {level}")
+        sys.exit(1)
+
+def configure_logger(log_filename, timestamp, log_level):
+    logger = logging.getLogger()
+
+    # Read filename details
+    dirname = os.path.dirname(log_filename)
+    filename, extension = os.path.splitext(os.path.basename(log_filename))
+    # Add timestamp in log filename
+    log_filename = os.path.join(dirname, f"{filename}.{timestamp}{extension}")
+    # Setup file handler logging
+    file_handler = logging.FileHandler(log_filename)
+
+    # Set log level
+    log_level = select_logging_level(log_level)
+    logger.setLevel(log_level)
+
+    # Add header formatting of the log message
+    logging.Formatter.converter = time.gmtime
+    formatter = logging.Formatter("[%(levelname)-8s] [%(asctime)s] %(message)s", datefmt="%Y/%m/%d %H:%M:%S")
+    file_handler.setFormatter(formatter)
+
+    # Add file handler
+    logger.addHandler(file_handler)
+
+    return logger
+
+def build_stakers(logger, num_stakers, whales, whales_stake, total_staked, distribution, coin_ageing, timestamp):
     if distribution == "random":
         stakers = [random.random() for s in range(num_stakers)]
     elif distribution == "uniform":
@@ -23,18 +66,28 @@ def build_stakers(num_stakers, total_staked, distribution):
         print("Unknown staking distribution")
         sys.exit(1)
 
-    stakers = {i: int(s / sum(stakers) * total_staked) for i, s in enumerate(stakers)}
+    stakers = {i: int(s / sum(stakers) * (total_staked * (100 - whales_stake) / 100)) for i, s in enumerate(stakers)}
+    num_whales = whales
+    while whales > 0:
+        stakers[num_stakers - whales] = int(whales_stake / 100 / num_whales * total_staked)
+        whales -= 1
 
-    fig, ax = plt.subplots(1, 1)
-    ax.hist([stake / 1E6 for stake in stakers.values()])
-    ax.set_xlabel("Staked (M)")
-    ax.set_ylabel("Number of stakers")
-    plt.title(f"Stakers = {num_stakers}, total staked = {int(total_staked / 1E6)}M, distribution = {distribution}")
-    plt.savefig(f"plots/stakers_stakers={num_stakers}_staked={int(total_staked / 1E6)}M_distribution={distribution}.png", bbox_inches="tight")
+    logger.info(f"Stakers: {stakers}")
+
+    plot_stakers(stakers, num_stakers, total_staked, distribution, coin_ageing, timestamp)
 
     return stakers
 
-def simulate_epoch(stakers, coin_age, replication):
+def plot_stakers(stakers, num_stakers, total_staked, distribution, coin_ageing, timestamp):
+    fig, ax = plt.subplots(1, 1)
+    ax.hist([stake / 1E6 for stake in stakers.values()], bins=32)
+    ax.set_xlabel("Staked (M)")
+    ax.set_ylabel("Number of stakers")
+    plt.title(f"Stakers = {num_stakers}, total staked = {int(total_staked / 1E6)}M, distribution = {distribution}, ageing = {coin_ageing}")
+    plt.savefig(f"plots/stakers_{timestamp}.png", bbox_inches="tight")
+
+def simulate_epoch(logger, epoch, stakers, coin_age, replication):
+    logger.info(f"Simulating epoch {epoch + 1}")
     # eligibility: vrf < (2 ** 256) * own_power / global_power * rf
 
     # Calculate global power
@@ -42,81 +95,116 @@ def simulate_epoch(stakers, coin_age, replication):
     global_average_age = int(sum(coin_age.values()) / len(coin_age))
     global_power = min(total_staked * global_average_age, 184_467_440_737_095_551_615)
 
-    eligibility = []
+    proposals = []
+    vrf = random.random()
+    logger.info(f"VRF target: {vrf}")
     for staker, stake in stakers.items():
         # Calculate power of the staker
         own_power = min(stake * coin_age[staker], 1_475_739_525_896_764_412)
+        eligibility = own_power / global_power * replication
+        if eligibility > 1.0:
+            logger.warning(f"Eligibility for staker {staker} exceeds 1.0: {eligibility}")
+        if vrf < eligibility:
+            block_hash = random.random()
+            logger.info(f"Staker {staker} is eligible to propose a block: {vrf} < {eligibility}, {block_hash}")
+            proposals.append((staker, block_hash))
 
-        vrf = random.random()
-        if vrf < own_power / global_power * replication:
-            eligibility.append((staker, vrf))
-
-    if len(eligibility) == 0:
+    if len(proposals) == 0:
+        logger.warning(f"No blocks proposed")
         return -1
     else:
         # Miner with the lowest VRF value is picked as the winner
-        miner = min(eligibility, key=lambda l: l[1])
+        miner = min(proposals, key=lambda l: l[1])
+        logger.info(f"Staker {miner[0]} is selected to propose a block: {miner[1]}")
         return miner[0]
 
-def update_coin_age(num_stakers, coin_age, miner):
+def update_coin_age(logger, num_stakers, coin_age, miner, coin_ageing):
     for staker in range(num_stakers):
         if staker != miner:
-            coin_age[staker] += 1
+            if coin_ageing == "capped":
+                coin_age[staker] = min(coin_age[staker] + 1, 1920)
+            else:
+                coin_age[staker] += 1
         else:
-            coin_age[staker] = 0
+            if coin_ageing in ("reset", "capped"):
+                coin_age[staker] = 0
+            elif coin_ageing == "halving":
+                coin_age[staker] /= 2
+            else:
+                print("Unknown coin age coin ageing algorithm")
+                sys.exit(1)
+
+    logger.info(f"Updated coin age: {coin_age}")
+
     return coin_age
+
+def print_mining_stats(logger, stakers, mined_blocks, epochs):
+    logger.info("Miner: blocks (percentage) -- stake (percentage)")
+    for staker, stake in sorted(stakers.items(), key=lambda l: l[1], reverse=True):
+        blocks = mined_blocks[staker] if staker in mined_blocks else 0
+        block_percentage = blocks / epochs * 100
+        stake_percentage = stake / sum(stakers.values()) * 100
+        logger.info(f"{staker}: {blocks} ({block_percentage:.2f}%) -- {stake / 1E6:.2f}M ({stake_percentage:.2f}%)")
+
+def plot_mining_rate(stakers, mined_blocks, epochs, num_stakers, total_staked, distribution, coin_ageing, timestamp):
+    block_percentages, stake_percentages = [], []
+    for staker, stake in sorted(stakers.items(), key=lambda l: l[1], reverse=True):
+        blocks = mined_blocks[staker] if staker in mined_blocks else 0
+        block_percentages.append(blocks / epochs * 100)
+        stake_percentages.append(stake / sum(stakers.values()) * 100)
+
+    fig, ax = plt.subplots(1, 1)
+    ax.scatter(stake_percentages, block_percentages)
+    ax.set_xlabel("Staked (%)")
+    ax.set_ylabel("Blocks mined (%)")
+    plt.title(f"Stakers = {num_stakers}, total staked = {int(total_staked / 1E6)}M, distribution = {distribution}, ageing = {coin_ageing}")
+    plt.savefig(f"plots/mining_{timestamp}.png", bbox_inches="tight")
 
 def main():
     parser = optparse.OptionParser()
     parser.add_option("--stakers", type="int", default=100, dest="num_stakers")
     parser.add_option("--total-staked", type="int", default=100000000, dest="total_staked")
     parser.add_option("--distribution", type="string", default="random", dest="distribution")
+    parser.add_option("--whales", type="int", default=0, dest="whales")
+    parser.add_option("--whales-stake", type="int", default=0, dest="whales_stake")
     parser.add_option("--epochs", type="int", default=100000, dest="epochs")
     parser.add_option("--replication", type="int", default=16, dest="replication")
-    parser.add_option("--verbose", action="store_true", dest="verbose")
+    parser.add_option("--coin-ageing", type="string", default="reset", dest="coin_ageing")
+    parser.add_option("--logging", type="string", default="info", dest="logging")
     options, args = parser.parse_args()
 
     if not os.path.exists("plots"):
         os.mkdir("plots")
 
-    stakers = build_stakers(options.num_stakers, options.total_staked, options.distribution)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    logger = configure_logger("mining", timestamp, options.logging)
+
+    logger.info(f"Command line arguments: {options}")
+
+    stakers = build_stakers(logger, options.num_stakers, options.whales, options.whales_stake, options.total_staked, options.distribution, options.coin_ageing, timestamp)
     # Everyone starts off with coin age equal to 1, otherwise no block will be mined in the first epoch
     coin_age = {staker: 1 for staker in range(options.num_stakers)}
+    logger.info(f"Initial coin age: {coin_age}")
 
     previous_miner = -1
     mined_blocks = {}
     for epoch in tqdm.tqdm(range(options.epochs)):
-        miner = simulate_epoch(stakers, coin_age, options.replication)
-        if miner != -1:
+        miner = simulate_epoch(logger, epoch, stakers, coin_age, options.replication)
+        if miner != -1 and options.coin_ageing != "halving":
             assert miner != previous_miner, "No miner should be able to mine two subsequent blocks"
         previous_miner = miner
 
-        coin_age = update_coin_age(options.num_stakers, coin_age, miner)
+        if options.coin_ageing != "disabled":
+            coin_age = update_coin_age(logger, options.num_stakers, coin_age, miner, options.coin_ageing)
 
         if miner not in mined_blocks:
             mined_blocks[miner] = 0
         mined_blocks[miner] += 1
 
-    block_percentages, stake_percentages = [], []
-    if options.verbose:
-        print("Miner: blocks (percentage) -- stake (percentage)")
-    for staker, stake in sorted(stakers.items(), key=lambda l: l[1], reverse=True):
-        blocks = mined_blocks[staker] if staker in mined_blocks else 0
-        block_percentage = blocks / options.epochs * 100
-        stake_percentage = stake / sum(stakers.values()) * 100
+    print_mining_stats(logger, stakers, mined_blocks, options.epochs)
 
-        block_percentages.append(block_percentage)
-        stake_percentages.append(stake_percentage)
-
-        if options.verbose:
-            print(f"{staker}: {blocks} ({block_percentage:.2f}%) -- {stake / 1E6:.2f}M ({stake_percentage:.2f}%)")
-
-    fig, ax = plt.subplots(1, 1)
-    ax.scatter(stake_percentages, block_percentages)
-    ax.set_xlabel("Staked (%)")
-    ax.set_ylabel("Blocks mined (%)")
-    plt.title(f"Stakers = {options.num_stakers}, total staked = {int(options.total_staked / 1E6)}M, distribution = {options.distribution}")
-    plt.savefig(f"plots/mining_stakers={options.num_stakers}_staked={int(options.total_staked / 1E6)}M_distribution={options.distribution}.png", bbox_inches="tight")
+    plot_mining_rate(stakers, mined_blocks, options.epochs, options.num_stakers, options.total_staked, options.distribution, options.coin_ageing, timestamp)
 
 if __name__ == "__main__":
     main()
